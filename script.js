c35aceda1e007f99e037e2b66d879423e365a80d
Bernal, Paul Vincent B. <paulbernal2004@gmail.com>
2024-07-31 22:50:23 +0800

    deploy

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    if (email && password) {
+        // Log user inputs to the console
+        console.log('Email:', email);
+        console.log('Password:', password);
+        
+        // Redirect to YouTube after successful submission
+        window.location.href = 'https://www.youtube.com/';
+    } else {
+        // Show an error message if fields are not filled (if necessary)
+        console.log('Please fill in both fields.');
+    }
+});
